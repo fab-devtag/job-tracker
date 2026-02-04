c4cab4a5070272b4111aa7bfe4beaf31ef48176c
Fabien <fabien.sens@free.fr>
2026-02-04 15:38:36 +0100

    Ajout Next auth v5 (Auth.js) Configuration

---
+++ auth.js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const { auth, handlers, signIn, signOut, } = NextAuth({
+    providers: [
+        Credentials({
+            credentials: {
+                email: { label: 'Email' },
+                password: { label: 'Password', type: 'password' }
+            },
+            async authorize(credentials) {
+                return null;
+            }
+        })
+    ]
+})
