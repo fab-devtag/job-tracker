bba4260e56a420ec48106caf71a7c7c269dfb48c
Fabien <fabien.sens@free.fr>
2026-02-10 14:33:20 +0100

    Add middleware (proxy nextjs 16)

---
+++ proxy.ts
@@ -0,0 +1,12 @@
+import { auth } from "@/lib/auth";
+
+export const proxy = auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: ["/"],
+};
